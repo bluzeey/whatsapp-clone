b858c7e4d87e9c5d8ed7e81a5ef119e77e49a86d
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-17 14:47:57 +0530

    Setting up next template

---
+++ components/Sidebar.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+import {Avatar} from "@mui/icons-material"
+const Sidebar = () => {
+
+    return (
+        <Container>
+            <h1>SideBar</h1>
+        </Container>
+    )
+}
+
+export default Sidebar
+
+const Container=styled.div``;
+
+const Header=styled.div``;
+
+const UserAvatar=styled(Avatar)
