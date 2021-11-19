e6c78488cd52002c3b59261388587ef985a03873
Sahil Maheshwari <sahilm1711@gmail.com>
2021-11-19 14:03:54 +0530

    Rectified packages and corrected the pages

--- firebase.js
+++ firebase.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-
-import firebase from 'firebase';
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import firebase from 'firebase'
 const firebaseConfig = {
   apiKey: "AIzaSyCa-gDWQ7s0aB5gPkVGzs0V105dy6c6BTY",
   authDomain: "whatsapp-clone-3b640.firebaseapp.com",
@@ -15,10 +8,8 @@ const firebaseConfig = {
   appId: "1:849543519665:web:1332495733047af2d6b190"
 };
 
-// Initialize Firebase
-const app =!firebase.apps.length? initializeApp(firebaseConfig):firebase.app();
+const app =!firebase.apps.length ? firebase.initializeApp(firebaseConfig): firebase.app();
 const db=app.firestore();
 const auth=app.auth();
-const provider=new firebase.auth.GoogleAuthProvider();
-
-export {db,auth,provider}
+const provider= new firebase.auth.GoogleAuthProvider();
+export {db, auth, provider}
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css'
-
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {auth,db} from '../firebase.js'
+import Login from './login'
 function MyApp({ Component, pageProps }) {
+  const [user,loading]=useAuthState(auth)
+  if(loading) return <Loading/>
+  if(!user) return <Login/>
+
   return <Component {...pageProps} />
 }
 
